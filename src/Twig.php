e7c481affae77b3b7d37d0eb7b2f493f416d7f3b
yunwuxin <448901948@qq.com>
2017-09-15 18:09:24 +0800

    fix view_path

--- src/Twig.php
+++ src/Twig.php
@@ -143,7 +143,7 @@ class Twig
             $this->config($config);
         }
 
-        $loader = new Twig_Loader_Filesystem(APP_PATH . $this->config['view_path']);
+        $loader = new Twig_Loader_Filesystem($this->config['view_path']);
 
         if (Config::get('app_multi_module')) {
             $modules = $this->getModules();
